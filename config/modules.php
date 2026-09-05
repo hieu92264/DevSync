9b24cc206131e14446631268bfd9ed71b45baa4a
hieu92264 <nguyenthehieu20182021@gmail.com>
2026-09-05 09:05:17 +0700

    Add module aliases for improved shorthand access

--- config/modules.php
+++ config/modules.php
@@ -4,5 +4,15 @@ return [
     'base_path' => app_path('Modules'),
     'namespace' => 'App\\Modules',
     //array for short name
-    'aliases' => []
+    'aliases' => [
+        'AI' => 'ai',
+        'Api' => 'api',
+        'Audit' => 'audit',
+        'Authorization' => 'authori',
+        'Identity' => 'identity',
+        'Observability' => 'obs',
+        'Organization' => 'org',
+        'Project' => 'proj',
+        'Task' => 'task',
+    ]
 ];
