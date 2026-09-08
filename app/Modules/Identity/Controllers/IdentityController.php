b2c421a12f36bdb1282f2a41a0758a265dc6d517
hieu92264 <nguyenthehieu20182021@gmail.com>
2026-09-08 15:17:46 +0700

    Add locale support and file handling features with middleware and helper classes

--- app/Modules/Identity/Controllers/IdentityController.php
+++ app/Modules/Identity/Controllers/IdentityController.php
@@ -7,12 +7,33 @@ use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class IdentityController extends Controller
 {
-
     use ApiResponse;
-    public function login(Request $request) {}
+
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+    public function login(Request $request): JsonResponse {
+        $request->validate([
+            'login' => ['required', 'string'],
+            'password' => ['required', 'string'],
+        ]);
+
+        $loginInput = $request->input('login');
+
+        $fieldType = filter_var($loginInput, FILTER_VALIDATE_EMAIL) ? 'email' : 'user_name';
+        $credentials = [$fieldType => $loginInput, 'password' => $request->password];
+
+        if(!$token = Auth::guard('api')->attempt($credentials)) {
+            return $this->error(null, 'Unauthorized', Response::HTTP_UNAUTHORIZED);
+        }
+
+        return $this->respondWithToken($token);
+    }
 
     public function logout(): JsonResponse
     {
@@ -20,5 +41,22 @@ class IdentityController extends Controller
         return $this->success(null, 'Logged out successfully', 200);
     }
 
-    public function me() {}
+    public function me() {
+        $user = Auth::guard('api')->user();
+        return $this->success($user, 'User retrieved successfully', 200);
+    }
+
+    public function refresh(): JsonResponse
+    {
+        return $this->respondWithToken(Auth::guard('api')->refresh());
+    }
+
+    protected function respondWithToken(string $token): JsonResponse
+    {
+        return $this->apiResponse([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => Auth::guard('api')->factory()->getTTL() * 60
+        ], 'Success', Response::HTTP_OK);
+    }
 }
--- app/Modules/Identity/Routes/index.php
+++ app/Modules/Identity/Routes/index.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Modules\Identity\Controllers\IdentityController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('auth')->group(function () {
-    //
+Route::prefix('auth')->controller(IdentityController::class)->group(function () {
+    Route::post('/login', 'login');
+    Route::post('/logout', 'logout');
+    Route::get('/refresh', 'refresh');
+    Route::get('/me', 'me');
 });
